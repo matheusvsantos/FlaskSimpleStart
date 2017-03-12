f25b3c7e958bfc26dfcbf5ebe5cc29c23b87eb44
Matheus Veleci dos Santos <matheusveleci@aluno.unb.br>
2017-03-12 13:23:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask_restful import Api
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'matheus'
@@ -14,4 +17,10 @@ api = Api(app)
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
